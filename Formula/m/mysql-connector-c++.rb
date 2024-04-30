db00e279fc46ea08ec6deda975582cecd25a32b7
Rui Chen <rui@chenrui.dev>
2024-04-30 13:01:26 -0400

    mysql-connector-c++ 8.4.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mysql-connector-c++.rb
+++ Formula/m/mysql-connector-c++.rb
@@ -1,8 +1,8 @@
 class MysqlConnectorCxx < Formula
   desc "MySQL database connector for C++ applications"
   homepage "https://dev.mysql.com/downloads/connector/cpp/"
-  url "https://dev.mysql.com/get/Downloads/Connector-C++/mysql-connector-c++-8.3.0-src.tar.gz"
-  sha256 "a17bf1fad12b1ab17f5f6c7766289fb87200e919453234c3ec1664d7734be8f8"
+  url "https://dev.mysql.com/get/Downloads/Connector-C++/mysql-connector-c++-8.4.0-src.tar.gz"
+  sha256 "540b3d3b4d20ecf9f900bf55bba870718e50672f54fa2cdb124adf385796ce8b"
   license "GPL-2.0-only" => { with: "Universal-FOSS-exception-1.0" }
 
   livecheck do
